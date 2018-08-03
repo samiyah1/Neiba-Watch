32a2d768956c0283c56247ec7a0c3e5997119565
Mariam <mariam@localhost.localdomain>
2018-08-03 11:06:47 +0300

    created the urlpattern for the home page

--- watch/neiba/urls.py
+++ watch/neiba/urls.py
@@ -5,7 +5,9 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns=[
-    url(r'^login/$', auth_views.login, {'template_name': 'registration/login.html'}, name='login'),
-    url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
+    url(r'^$',views.home,name = 'home'),
     url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', auth_views.login, {'template_name': 'registration/login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout')
+
 ]
