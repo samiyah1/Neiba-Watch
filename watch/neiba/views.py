cef740984d80e30c063ed7d3af9441515d38eba1
Mariam <mariam@localhost.localdomain>
2018-08-03 05:17:31 +0300

    created a view method to allow users to login the neighbourhood app

--- watch/neiba/views.py
+++ watch/neiba/views.py
@@ -7,3 +7,23 @@ from .forms import LoginForm, UserRegistrationForm,
 from django.contrib import messages
 
 # Create your views here.
+def user_login(request):
+    """
+    This a view method to allow users to login the neighbourhood app
+    """
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authenticated successfully')
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+    return render(request, 'registration/login.html', {'form': form})
