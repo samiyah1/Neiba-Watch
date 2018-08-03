bebe468f86f2957a871a42c58697b3b79a5f6a5d
Mariam <mariam@localhost.localdomain>
2018-08-03 11:19:11 +0300

    added a media root and also created url pattern for profile

--- watch/neiba/urls.py
+++ watch/neiba/urls.py
@@ -8,6 +8,9 @@ urlpatterns=[
     url(r'^$',views.home,name = 'home'),
     url(r'^register/$', views.register, name='register'),
     url(r'^login/$', auth_views.login, {'template_name': 'registration/login.html'}, name='login'),
-    url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout')
+    url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
+    url(r'^profile/$',views.Profile,name = 'userprofile'),
 
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
