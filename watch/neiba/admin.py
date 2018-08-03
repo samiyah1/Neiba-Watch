b9c994880417495a382278ab659162874e9a8084
Mariam <mariam@localhost.localdomain>
2018-08-03 14:22:03 +0300

    added the profile class in the admin dashbord

--- watch/neiba/admin.py
+++ watch/neiba/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import NeighbourHood
+from .models import NeighbourHood,Profile
 # Register your models here.
 admin.site.register(NeighbourHood)
+admin.site.register(Profile)
