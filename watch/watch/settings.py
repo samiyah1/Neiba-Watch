a8cbc3fd0727126d58569c888b9f2c6d7baf63fc
Mariam <mariam@localhost.localdomain>
2018-08-03 05:23:11 +0300

    installed bootstrap,tinymce,psycopg2a  and created a database in the settings

--- watch/watch/settings.py
+++ watch/watch/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'watch.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+         'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'watch',
+        'USER': 'mariam',
+    'PASSWORD':'samiyah1',
     }
 }
 
