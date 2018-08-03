48a30043a76c81bb7a93fbe829ef314f3bfe52e6
Mariam <mariam@localhost.localdomain>
2018-08-03 05:12:10 +0300

    imported messages,authentication ,httpresponses in the apps view file

--- watch/neiba/views.py
+++ watch/neiba/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http  import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .forms import LoginForm, UserRegistrationForm,
+from django.contrib import messages
 
 # Create your views here.
