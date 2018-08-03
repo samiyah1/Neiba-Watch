1693ebd461bd17973cb82674e8dbafad87c0d9cd
Mariam <mariam@localhost.localdomain>
2018-08-03 05:19:01 +0300

    added a loginform

--- watch/neiba/forms.py
+++ watch/neiba/forms.py
@@ -3,7 +3,9 @@ from django.conf import settings
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
 
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(label='Password', widget=forms.PasswordInput)
