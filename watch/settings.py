6fe13d5bcdee7a36e76b0a058b2dbed8560f435e
Mariam <mariam@localhost.localdomain>
2018-08-05 17:21:39 +0300

    adding a heroku app in the allowed host

--- watch/settings.py
+++ watch/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['neiba-watch']
+ALLOWED_HOSTS = ['neiba-watch.herokuapp.com']
 
 
 # Application definition
